65c8ab734b004eae9ea2cd769161b576decc7f27
MatheusAFPimentel <matheus.algustt@gmail.com>
2020-07-28 12:58:55 -0300

    Atualizações aula 02

---
+++ src/components/Pagina404/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import PageDefault from '../PageDefault'
+
+function Pagina404() {
+    return (
+        <div>
+            <PageDefault>
+                Pagina 404
+            </PageDefault>
+        </div>
+    )
+}
+
+export default Pagina404
