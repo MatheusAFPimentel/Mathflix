5bd840de3a9b0ca159bb7c56e6fe3c3be016d735
MatheusAFPimentel <matheus.algustt@gmail.com>
2020-08-04 15:51:15 -0300

    Arrumando detalhes do Footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -7,6 +7,7 @@ function Footer() {
       <a href="https://www.youtube.com/watch?v=E6-qAjx5cMY">
       <img src="https://img.icons8.com/doodle/48/000000/walter-white.png"/>
       </a>
+      <p>Produzido por: <a href="https://www.linkedin.com/in/matheus-augusto-33077194/"> Matheus Augusto </a></p> 
     </FooterBase>
   );
 }
