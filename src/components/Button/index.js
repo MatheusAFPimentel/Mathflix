d7082162aceca74bc4d2644ea173ab29cca40273
MatheusAFPimentel <matheus.algustt@gmail.com>
2020-08-04 16:25:03 -0300

    Colocando responsividade

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,27 +1,20 @@
 import styled from 'styled-components'
 
 const Button = styled.button`
-color: black;
-border: 1px solid #fff200;
-box-sizing: border-box;
-cursor: pointer;
-padding: 16px 24px;
-font-style: normal;
-font-weight: bold;
-font-size: 20px;
-outline: none;
-border-radius: 5px;
-text-decoration: none;
-display: inline-block;
-transition: opacity .3s;
-background-color: #fff200;
-left: 0;
-top: 0;
-fill: none;
-stroke: #fff;
-stroke-dasharray: 150 480;
-stroke-dashoffset: 150;
-transition: 0.35s ease-in-out;
+  color: black;
+  border: 1px solid var(--white);
+  background: #fff200;
+  box-sizing: border-box;
+  cursor: pointer;
+  padding: 16px 24px;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 16px;
+  outline: none;
+  border-radius: 5px;
+  text-decoration: none;
+  display: inline-block;
+  transition: opacity .3s;
 
 
 &:hover{
